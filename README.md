7c4f81f355f9745e04d77a372f5417b8b293dd9b
dejongbrian <bochieng25@gmail.com>
2019-10-11 20:56:38 +0300

    update README.md

--- README.md
+++ README.md
@@ -1,26 +1,35 @@
-# DEJONGS' PALACE
- 
+# DeJongs' Palace
+
+#### An application that helps user select their pizza preferances and have it delivered at their door step, 2.0
+#### By **[Brian Ochieng]**
 ## Description
- A web page that enables the user to view types of Pizza and make orders online..
+This is a web application that allows a user to place an order depending on the pizza size they'd like, the crust and also the kind of toppings they want o their pizza. It also allows useer to select the number of pizzas they'd like. Finally, the user gets to choose whether they'd want their pizza delivered or not, if they want it delivered, they are required to enter their location and add a little delivery fee.
+Go ahead and try! Have your Pizza delivered in less than 40 minutes!
+## screenshot
 
-### Author
- BRIAN OCHIENG 
-
- 
-#### Technologies Used
-* html5
+## Setup/Installation Requirements
+There are no set up or installation reqiurements
+## Known Bugs
+The application is not able to take orders of more than 5 pizzas at a go.
+## Technologies Used
+ * html5
 * css
 * Javascript
 * JQuery
 * Bootstrap CSS
-
-
-###### Setup/Installation Requirements
-* Open dejongbrian profile on GitHUb .
-* Click on Dejongs' Palace repository.
-* Click on the link displayed next to the description.
-
-
+* Markdown.
+## BDD
+| Behaviour      | Input        | Output       |
+| :------------- | :----------: | -----------: |
+|  Enter your name  |   Brian Ochieng |     |
+| Enter your Email Address  | bochieng25@gmail.com |   |
+| Enter your pizza size,crust type and toppings   |  small, thin crust Chicken   |  summary of order made   ||
+|Delivery option| yes|your order will be delivered at Kmall
+| Press Submit|     | 
 ## Support and contact details
-* Tel:+254 713 786 896
-* Email:bochieng25@gmail.com
+Contribution and improvements to the application are welcome.
+ Feel free to reach out to 
+ * Tel:+254 713 786 896
+ * Email:bochieng25@gmail.com
+
+
--- js/script.js
+++ js/script.js
@@ -1,17 +1,46 @@
 
-$(document).ready(function(){
-  $('.carousel').carousel({
-    interval: 1000
-    })
-  });
+// $(document).ready(function(){
+//   $('.carousel').carousel({
+//     interval: 1000
+//     })
+//   });
    //User //
    
-   $("button#order").click(function(){
-       var inputSize = $("select#size").val();
-       var inputCrust = $("select#crust").val();
-       var inputQuant = $("input#quantity").val();
-       var inputToppings = $("input#topp").val();
-       var newPizza = new Pizza(inputSize, inputCrust, inputQuant, inputToppings)
-      
-       $("p#message").append("Your total will be: Ksh. "+newPizza.getCost());
+   function updateTotal($form)
+   {
+      var total = 0;
+      var selectedsize = $form.find('input[name="selectedsize"]:checked').data('price');
+      if (!isNaN(selectedsize))
+      {
+          total += selectedsize;
+      }
+      var topping=$form.find('input[name="selectedtopping"]:selected').data('price');
+      if (!isNaN(selectedtopping))
+      {
+          total += selectedtopping;
+      }
+      var selectedcrust = $form.find('select[name="selectedcrust"] option:ckecked').data('price');
+      if (!isNaN(selectedcrust))
+      {
+          total += selectedcrust;
+      }
+      $form.find('input[name="total"]').val(total);
+   }
+   $(document).ready(function() {
+      $('#sizeform1 input[name="selectedsize"]').change(function () {
+          var $form = $(this).closest('form');
+          updateTotal($form);
       });
+      $('#toppingform1 input[name="selectedtopping"]').change(function (){
+          var $form= $(this).closest('form');
+          updateTotal($form);
+      });
+      $('#crustform1 select[name="selectedcrust"]').change(function () {
+          var $form = $(this).closest('form');
+          updateTotal($form);
+      });
+   
+      $("#deliver").click(function(){
+        alert("Your order will be delivered within 40min stay updated will deliver soon")
+      })
+   });
