672e78e5c6abfceda58390e77cb4106f9cb2e308
dejongbrian <bochieng25@gmail.com>
2019-10-12 18:07:46 +0300

    make changes

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
-# DEJONGS' PALACE
- 
+# DeJongs' Palace
+
+#### An application that helps user select their pizza preferances and have it delivered at their door step, 2.0
+#### By **[Brian Ochieng]**
 ## Description
- A web page that enables the user to view types of Pizza and make orders online..
-
-### Author
- BRIAN OCHIENG 
-
- 
-#### Technologies Used
-* html5
-* css
-* Javascript
+This is a web application that allows a user to place an order depending on the pizza size they'd like, the crust and also the kind of toppings they want placed on their pizza. It also allows useer to select the number of pizzas they'd like. Finally, the user gets to choose whether they'd want their pizza delivered or not, if they want it delivered, they are required to enter their location and add a little delivery fee. Go ahead and try! Have your Pizza delivered in less than 40 minutes!
+## screenshot
+/Dejongs/images/Screenshot from 2019-10-12 16-44-47.png
+## Setup/Installation Requirements
+There are no set up or installation reqiurements
+## Known Bugs
+Apart from being able to take a maximum of 10 pizzas in quantity, there is no other bug.
+## Technologies Used
+* HTML5
+* CSS
+* JAVASCRIPT
 * JQuery
 * Bootstrap CSS
+* Markdown.
+## BDD
+| Behaviour      | Input        | Output       |
+| :------------- | :----------: | -----------: |
 
-
-###### Setup/Installation Requirements
-* Open dejongbrian profile on GitHUb .
-* Click on Dejongs' Palace repository.
-* Click on the link displayed next to the description.
-
-
+| Choose your pizza size,crust type and toppings   |  Medium,Mushroom topping & Cheesy crust.   |  summary of order made   ||
+|Delivery option| yes|your order will be delivered at Kmall
+| Press Submit|     | 
 ## Support and contact details
-* Tel:+254 713 786 896
-* Email:bochieng25@gmail.com
+Contribution and improvements to the application are welcome.
+ Feel free to reach out to 
+ * Tel:+254 713 786 896
+ * Email:bochieng25@gmail.com
+
+
--- js/script.js
+++ js/script.js
@@ -1,104 +1,47 @@
+// Carousel Interface
 $(document).ready(function(){
-$('.carousel').carousel({
-  interval: 1000
-  })
-});
- 
-  var pizza=["size","topping","crust","quantity"]
-  var size=["small","medium","large","x-large"]
-  var topping=["mushrooms","pineapple","olives","bell pepper","ham","chicken pieces"]
-  var crust=["Thin & Crispy","Deep Pan","Classic Crust","Cheesy Crust","Gluten free crust"]
-  var sizePrice=[0]
-
-function Pizza(topping,size,crust,quantity){
-  this.selectedTopping=topping;
-  this.selectedSize=size;
-  this.selectedCrust=crust;
-  this.selectedQuantity=quantity;
-}
-
-
-function sizePrice(topping,size,crust){
-  return this.selectedSize + this.selectedTopping + this.selectedCrust  
-}
-
-
-
-function Total(sizePrice,quantity,delivery){
-  this.sizePrice=sizePrice;
-  this.selectedQuantity=quantity;
-  this.delivery=delivery;
-}
-
-
-//sizePrice//
- pizza.prototype.sizePrice=function(){
-   var sizePrice=0;
-   if (this.sizePrice==="small"){
-     return sizePrice =+1;
-   } else if (this.sizePrice==="medium"){
-     return sizePrice =+2;
-   }else if (this.sizePrice==="large"){
-     return sizePrice =+3;
-   }else if (this.sizePrice==="x-large"){
-     return sizePrice =+4;
-   }else{
-     return sizePrice =+0;
-   }
- }
-//pizzaToppings//
-pizza.prototype.topping=function(){
-  var topping=0;
-  if(this.topping==="0"){
-    return topping = "mushrooms";
-  }else if(this.topping==="1"){
-    return topping="pineapple";
-  }else if(this.topping==="2"){
-    return topping="olives";
-  }else if(this.topping==="3"){
-    return topping="bell pepper";
-  }else if(this.topping==="4"){
-    return topping="ham"
-  }else if(this.topping==="5"){
-    return topping="chicken pieces";
-  }else{
-    return topping="none";
-  }
-}
-//pizza crust//
-pizza.prototype.crust=function(){
-  var crust=0;
-  if(this.crust==="0"){
-    return crust="Thin & Crispy";
-  }else if(this.crust==="1"){
-    return crust="Deep Pan";
-  }else if(this.crust==="2"){
-    return crust="Classic Crust";
-  }else if(this.crust==="3"){
-    return crust="Cheesy Crust";
-  }else if(this.crust==="4"){
-    return crust="Gluten free crust";
-  }else{
-    return crust="none;"
-  }
-}
-
-
-//user logic//
-
-$(document).ready(function(){
-  var selectedtopping =(".topping option:selected").val();
-  var selectedsize =(".size option:selected").val();
-  var selectedsrust =(".crust option:selected").val();
- 
+    $('.carousel').carousel({
+      interval: 1000
+      })
+    });
   
-});
-
-function order(){
-    alert ("Your order is" +"" );
+  
+  
+  //Business Interface
+  function ShowSummary(getSize,getCrust,getTopping){                                                                                             
+      this.size = getSize;
+     this.crust = getCrust;
+      this.topping = getTopping;
   }
-
-
-
-
+  ShowSummary.prototype.totalSummary=function(){
+     return this.getsize + "" + this.getcrust + " " + this.getTopping;
+  }
+  var sizes= ["small", "medium", "large","x-large"]
+  $(document).ready(function(){
+   var Amount = 0;
+   $("#order").click(function(){
+     var checkVal = $("input[name=Yes]:checked").val();
+     if(checkVal === "yes") {
+       Amount = 400;
+       var getLocation = prompt("What is your location?");
+       alert("Dear customer your order will be delivered at " +  getLocation  +  "  you will pay an extra 400 Ksh for delivery fee.");
+     }
+   })
+  
+   //User Interface
+   $("#order").click(function(){
+     var Size = parseFloat(document.getElementById("size").value);
+     var Crust = parseFloat(document.getElementById("crust").value);
+     var Topping = parseFloat(document.getElementById("topping").value);
+     var Quantity = parseFloat(document.getElementById("num").value);
+     var Total = ((Size + Crust + Topping) * Quantity + Amount);
+     alert("You will pay " + Total + " Ksh amount of money.")
+     document.getElementById("getsize").innerHTML = Size;
+     document.getElementById("getcrust").innerHTML = Crust;
+     document.getElementById("gettopping").innerHTML = Topping;
+     document.getElementById("sum").innerHTML = Total;
+     $(".the-order").show();
+    
+   });
+  });
   
